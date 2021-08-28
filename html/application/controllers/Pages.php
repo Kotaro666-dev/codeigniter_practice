db4230e8776c04d88edb7d9a83e7da53ae76ab0f
Kotaro Kamashima <kotaro0726@gmail.com>
2021-08-28 13:11:41 +0900

    Update: Add pages

---
+++ html/application/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+class Pages extends CI_Controller
+{
+    public function view($page = 'home') {
+        if (!file_exists(APPPATH.'views/pages/'.$page.'.php')) {
+            show_404();
+        }
+
+        // ucfirst — 文字列の最初の文字を大文字にする
+        $data['title'] = ucfirst($page);
+        $this->load->view('templates/header');
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer');
+    }
+}
---
+++ html/application/views/templates/header.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title> CodeIgniter 3 practice </title>
+<!--        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@3.4.0/cosmo/bootstrap.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="<?php echo base_url(); ?>">CodeIgniter practice</a>
+            </div>
+            <div id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php echo base_url() ?>">Home</a></li>
+                    <li><a href="<?php echo base_url() ?>about">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
