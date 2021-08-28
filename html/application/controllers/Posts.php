2394eb068597138d98625086f3c2d9bfd0fffa0d
Kotaro Kamashima <kotaro0726@gmail.com>
2021-08-28 15:20:10 +0900

    Update: Add feature to jump to page link through read more button

--- html/application/controllers/Posts.php
+++ html/application/controllers/Posts.php
@@ -1,13 +1,6 @@
 <?php
     class Posts extends CI_Controller
     {
-//        public function __construct()
-//        {
-//            parent::__construct();
-//            $this->load->model('Posts_model');
-//        }
-
-
         public function index(){
             $data['title'] = 'Latest Posts';
 
@@ -16,5 +9,20 @@
             $this->load->view('templates/header');
             $this->load->view('posts/index', $data);
             $this->load->view('templates/footer');
+        }
+
+        public function view($slug = NULL)
+        {
+            $data['post'] = $this->post_model->get_posts($slug);
+
+            if (empty($data['post'])) {
+                show_404();
+            }
+
+            $data['title'] = $data['post']['title'];
+            $this->load->view('templates/header');
+            $this->load->view('posts/view', $data);
+            $this->load->view('templates/footer');
+
         }
     }
