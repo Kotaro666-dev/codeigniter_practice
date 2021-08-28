ee15afd58895fa6d86a9d748967a11174f9f34a2
Kotaro Kamashima <kotaro0726@gmail.com>
2021-08-28 15:05:14 +0900

    Update: Add posts navbar

--- html/application/views/templates/header.php
+++ html/application/views/templates/header.php
@@ -15,6 +15,7 @@
                 <ul class="nav navbar-nav">
                     <li><a href="<?php echo base_url() ?>">Home</a></li>
                     <li><a href="<?php echo base_url() ?>about">About</a></li>
+                    <li><a href="<?php echo base_url() ?>posts">Blog</a></li>
                 </ul>
             </div>
         </div>
