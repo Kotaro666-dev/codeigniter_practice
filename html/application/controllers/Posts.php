3379a4dfe95a048c3fc109a45cb5277b552229a7
Kotaro Kamashima <kotaro0726@gmail.com>
2021-08-28 14:38:47 +0900

    Update: Load database model

--- html/application/controllers/Posts.php
+++ html/application/controllers/Posts.php
@@ -4,6 +4,8 @@
         public function index(){
             $data['title'] = 'Latest Posts';
 
+            $data['posts'] = $this->load->model('Post_model');
+
             $this->load->view('templates/header');
             $this->load->view('posts/index', $data);
             $this->load->view('templates/footer');
