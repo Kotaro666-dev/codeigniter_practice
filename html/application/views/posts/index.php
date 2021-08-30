585075ea67ae1f02593c161b62319a32b2c32a88
Kotaro Kamashima <kotaro0726@gmail.com>
2021-08-31 08:11:10 +0900

    Update: Add pagination

--- html/application/views/posts/index.php
+++ html/application/views/posts/index.php
@@ -14,5 +14,8 @@
             <p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read more</a></p>
         </div>
     </div>
-
 <?php endforeach; ?>
+
+<div class="pagination-links">
+    <?php echo $this->pagination->create_links(); ?>
+</div>
